1130992577eb3eeed9207281e8bff37c773f6896
Jakub Belescak <jakub.belescak@centrum.cz>
2023-12-28 19:13:25 +0100

    fixing setup script so that it correctly installs the requirements

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+# Function to read the contents of requirements.txt
+def load_requirements(filename='requirements.txt'):
+    with open(filename, 'r') as file:
+        return [line.strip() for line in file.readlines() if not line.startswith('#') and line.strip()]
+
+
 setuptools.setup(
     name="shelllangchain",
-    version="0.1.6",
+    version="0.1.7",
     author="Belda",
     author_email="jakub.belescak@centrum.cz",
     description="Natural language to shell command converter",
@@ -26,7 +32,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.4',
-    install_requires = [
-        ['langchain', 'pyperclip', 'requests','dotenv']
-    ]
+    install_requires = load_requirements(),
 )
